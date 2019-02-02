c29256447d777396e7f1d246093ddf8d01321bf5
Matovu Ronald <matovuronald997@gmail.com>
2019-02-02 15:15:45 +0300

    Add Build Control Component

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const buildControls = (props) => (
+    <div>
+        build Controls
+    </div>
+);
+
+export default buildControls;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -15,8 +15,8 @@ class BurgerBuilder extends Component {
         ingredients: {
             salad: 0,
             bacon: 0,
-            cheese: 0,
-            meat: 0
+            cheese: 2,
+            meat: 1
         }
 
     };
